736b2e1f2a0fbb63c6cacb4b339d414dfcb749c3
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-06 10:25:06 -0400

    Add files via upload

---
+++ main (53).py
@@ -0,0 +1,17 @@
+from tabulate import tabulate
+data = [["Name", "Company", "Price"], 
+["S21", "Samsung", "800"], 
+["Mi 12", "Xiaomi", "600"], 
+["iPhone 12", "Apple", "700"]]
+print(tabulate(data))
+
+budget = [["Name", "Company", "Price"], 
+["Mi 10T", "Xiaomi", "400"], 
+["A52 5G", "Samsung", "300"], 
+["K3", "Oppo", "500"]]
+print(tabulate(budget))
+
+cheap = [["Name", "Company", "Price"], 
+["w41 pro", "LG", "180"], 
+["Nord 2 5G", "OnePlus", "410"],
+print(tabulate(budget))
